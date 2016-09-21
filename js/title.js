2d5470bc154610fce630c3373fe765911dd6c33b
Jorge Figueroa <yknx.4.b@gmail.com>
2016-09-21 10:55:38 -0500

    rebuilding site Wed Sep 21 10:55:38 CDT 2016

--- js/title.js
+++ js/title.js
@@ -1,12 +1,19 @@
 (function(){
+
+  function replaceTitle(element){
+    oldHtml = element.innerHTML;
+    newHtml = oldHtml.replace('Y k', 'A l e');
+    element.innerHTML = newHtml;
+  }
+
   var fiftyfifty = Math.random() > 0.5;
 
   if(fiftyfifty){
     var brandElement = document.getElementsByClassName('brand')[0];
+    var title = document.getElementsByTagName('title')[0];
 
-    oldHtml = brandElement.innerHTML;
-    newHtml = oldHtml.replace('Y k', 'A l e');
-    brandElement.innerHTML = newHtml;
+    replaceTitle(brandElement);
+    replaceTitle(title);
 
   }
 }());
