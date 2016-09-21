3f49187650590c1f8ecb107a4fd0c09a0aa357ce
Jorge Figueroa <yknx.4.b@gmail.com>
2016-09-21 10:39:37 -0500

    rebuilding site Wed Sep 21 10:39:37 CDT 2016

---
+++ js/title.js
@@ -0,0 +1,13 @@
+(function(){
+  var fiftyfifty = Math.random() > 0.5;
+
+  if(fiftyfifty){
+    var brandElement = document.getElementsByClassName('brand')[0];
+
+    brandElement.onload = function() {
+      oldHtml = brandElement.innerHtml;
+      newHtml = olHtml.replace('Y k', 'A l e');
+    }
+
+  }
+}());
